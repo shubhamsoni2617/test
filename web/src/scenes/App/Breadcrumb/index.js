f4d3de3460a9ae47be1bca21e722cd1179aff687
Jeetendra singh <jeetendra.singh@ranosys.com>
2019-07-23 16:48:24 +0530

    Added page title and bracrumb

--- web/src/scenes/App/Breadcrumb/index.js
+++ web/src/scenes/App/Breadcrumb/index.js
@@ -6,7 +6,20 @@ export default class Breadcrumb extends Component {
    render() {
 
       return (
-          <div>Breadcrumb</div>
+        <section className="banner">
+            <div className="banner-wrapper">
+                <img src="assets/images/promotions-banner.png" className="img-fluid" alt="page-banner" />
+            </div>
+            <div className="banner-overlay">
+            <div className="container-fluid">
+                <h1>Promotions (24)</h1>
+                <ul className="breadcrumb">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Promotion</a></li>
+                </ul>
+            </div>
+            </div>
+        </section>
       )
    }
 }
