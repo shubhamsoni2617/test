81e971c2ca03cefb5442b6ea78abd4924c635a62
Jeetendra singh <jeetendra.singh@ranosys.com>
2019-07-23 16:59:27 +0530

    Done the responsive of beadcrump and page title banner

--- web/src/scenes/App/Breadcrumb/index.js
+++ web/src/scenes/App/Breadcrumb/index.js
@@ -6,18 +6,18 @@ export default class Breadcrumb extends Component {
    render() {
 
       return (
-        <section className="banner">
+        <section className="breadcrumbs">
             <div className="banner-wrapper">
                 <img src="assets/images/promotions-banner.png" className="img-fluid" alt="page-banner" />
             </div>
             <div className="banner-overlay">
-            <div className="container-fluid">
-                <h1>Promotions (24)</h1>
-                <ul className="breadcrumb">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">Promotion</a></li>
-                </ul>
-            </div>
+                <div className="container-fluid">
+                    <h1>Promotions (24)</h1>
+                    <ul className="breadcrumb">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Promotion</a></li>
+                    </ul>
+                </div>
             </div>
         </section>
       )
