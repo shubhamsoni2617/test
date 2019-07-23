987c6ed2f7ab89434b02c4b309367d0e839b39fd
vandana <vandana.agrawal@ranosys.com>
2019-07-23 19:47:08 +0530

    page not found component created

--- web/src/scenes/App/routes.js
+++ web/src/scenes/App/routes.js
@@ -1,6 +1,7 @@
 import Home from '../Home'
 import Event from '../Event'
 import HomeService from '../../shared/services/HomeService'
+import PageNotFound from '../PageNotFound/index.1';
 
 const routes =  [
   {
@@ -10,8 +11,12 @@ const routes =  [
   },
   {
     path: '/event',
+    exact :true,  
     component: Event,
     fetchInitialData: (path = '') => HomeService.getData()
+  },
+  {
+    component: PageNotFound
   }
 ]
 
