77f18c3e93c48237d212c7b8e4a7a079f6985a93
Jeetendra singh <jeetendra.singh@ranosys.com>
2019-07-25 15:22:23 +0530

    Fixed promotion slider design

--- web/src/scenes/Home/PromotionCarousel/index.js
+++ web/src/scenes/Home/PromotionCarousel/index.js
@@ -85,7 +85,40 @@ export default class PromotionCarousel extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 3
+      slidesToScroll: 3,
+      customPaging: (i) => {
+            return (
+                <div className="dots-group">
+                    <span><a href="/"></a></span>
+                </div>
+            );
+        },
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     };
     return (
       <section className="promotions">
