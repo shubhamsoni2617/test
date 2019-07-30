4bab2196a631ceb4c5cf00ab6bd175d47afb57a6
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-07-30 11:05:46 +0530

    New baseurl update

--- web/src/shared/constants/index.js
+++ web/src/shared/constants/index.js
@@ -1,6 +1,6 @@
 class Constants {
 	static WEBSITE_URL = 'http://sistic.com';
-	static BASE_URL = '/sistic_dev/docroot/';
+	static BASE_URL = '/sistic/docroot/';
 	static INSTAGRAM_ACCESS_TOKEN = '3225660226.f09c095.d66beeb477664e4091320bcfe6e3991a';
 }
 
