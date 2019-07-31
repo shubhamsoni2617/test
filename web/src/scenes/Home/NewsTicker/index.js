305dc3d31a2fa6339c58d352b0787c8b6615ae2e
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-07-31 11:08:56 +0530

    NewsTicker multiple row problem resolved

--- web/src/scenes/Home/NewsTicker/index.js
+++ web/src/scenes/Home/NewsTicker/index.js
@@ -1,48 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import closeNews from '../../../assets/images/close-ad.svg';
 import './style.scss';
 import HomeService from '../../../shared/services/HomeService';
 
 
-class NewsTicker extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            newsTicker: []
-        };
-    }
+const NewsTicker = (props) => {
 
-    componentDidMount() {
+    const [newsTicker, setNewsTicker] = useState([]);
+    const [ticker, setTicker] = useState(true);
+
+    useEffect(() => {
         HomeService.getNewsTicker()
-        .then((res) => {
-            this.setState({ newsTicker: res.data.data })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }
+            .then((res) => {
+                setNewsTicker(res.data.data);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
-    render() {
 
-        const { newsTicker } = this.state;
-
-        return (
-            <div className="ticker-wrap" onClick={() => this.setState({ ticker: false })}>
-                <div className="ticker-container">
-                    <div className="ticker">
-                        {
-                            newsTicker.map((content, index) => {
-                                return (
-                                    <div key={content.title} className="ticker__item" dangerouslySetInnerHTML={{ __html: content.description }}></div>
-                                );
-                            })
-                        }
+    return (
+        <>
+            {
+                ticker
+                    ?
+                    <div className="ticker-wrap" >
+                        <div className="ticker-container">
+                            <div className="ticker">
+                                {
+                                    newsTicker.map((content, index) => {
+                                        return (
+                                            <div key={content.title} className="ticker__item" dangerouslySetInnerHTML={{ __html: content.description }}></div>
+                                        );
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <span className="close-ticker" onClick={() => setTicker(false)}><img src={closeNews} alt="Close" /></span>
                     </div>
-                </div>
-                <a href="/" className="close-ticker"><img src={closeNews} alt="Close" /></a>
-            </div>
-        );
-    }
+                    :
+                    null
+            }
+
+        </>
+    );
 }
 
 export default NewsTicker;
