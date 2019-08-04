b345fc3bae7d8061cafb052b2cf34494d408ac1c
Jeetendra singh <jeetendra.singh@ranosys.com>
2019-08-04 16:05:46 +0530

    Fixed the design of detail page

--- web/src/shared/components/PopUpWithClose/index.js
+++ web/src/shared/components/PopUpWithClose/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-
+import popupClose from '../../../assets/images/cross.svg';
+import './style.scss';
 
 const PopUpWithClose = (props) => {
     const {content, handleClose, title } = props;
     return (
-        <div>
-            <span onClick={()=>handleClose()}>Close</span>
-            <div>{title}</div>
-            <div>{content}</div>
+        <div className="modal" id="exampleModal">
+            <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg " role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
+                        <button type="button" className="close" onClick={()=>handleClose()}>
+                            <span aria-hidden="true"><img src={popupClose} alt="Close Popup" /></span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="notice">
+                            {content}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 } 
