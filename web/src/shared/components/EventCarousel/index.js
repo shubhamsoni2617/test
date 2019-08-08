36c4432a5641343d1aced394f2634e250dc96527
vandana <vandana.agrawal@ranosys.com>
2019-08-08 12:50:01 +0530

    Event Carousel slider chnages

--- web/src/shared/components/EventCarousel/index.js
+++ web/src/shared/components/EventCarousel/index.js
@@ -17,68 +17,68 @@ const EventCarousel = (props) => {
     setNavSmall(slider2);
   }, [])
 
-  let slider1 ={};
-  let slider2 ={};
+  let slider1 = {};
+  let slider2 = {};
+
+
 
   const settings1 = {
     nextArrow: <Arrow />,
     prevArrow: <Arrow />,
     className: "center",
-    centerMode: true,
+    variableWidth: true
   }
 
   const settings2 = {
     className: "center",
     centerMode: true,
+    variableWidth: true,
+    infinite: false,
+    focusOnSelect: true
   }
 
   const { images } = props;
 
   return (
-      <div className="banner-carousel">
+    <div className="banner-carousel">
+      <Slider
+        asNavFor={navSmall}
+        ref={slider => (slider1 = slider)}
+        {...settings1}
+      >
+        {images.map((obj, idx) => {
+          if (obj.type && obj.type.id == 1) {
+            return <div key={idx} style={{ width: '100%' }}>
+              <img src={obj.full_image} />
+            </div>
+          } else {
+            return <div className="videoimg" key={idx} style={{ width: '100%' }}>
+              <img src={obj.full_image} />
+              <ReactPlayer url={obj.video_url} controls={true} />
+            </div>
+          }
+        })}
+      </Slider>
+      <div className="carousel-thumbnails">
         <Slider
-          asNavFor={navSmall}
-          ref={slider => (slider1 = slider)}
-          {...settings1}
-          variableWidth ={true}
-          adaptiveHeight={true}
+          asNavFor={navLarge}
+          ref={slider => (slider2 = slider)}
+          {...settings2}
         >
           {images.map((obj, idx) => {
             if (obj.type && obj.type.id == 1) {
-              return <div key={idx}>
-                <img src={obj.full_image} />
+              return <div key={idx} style={{ width: 130 }}>
+                <img src={obj.thumb_image} />
               </div>
             } else {
-              return <div className="videoimg" key={idx}>
-                <ReactPlayer url={obj.video_url} controls={true} width='100%' height='100%' />
+              return <div className="videoimg" key={idx} style={{ width: 130 }} >
+                <img src={obj.thumb_image} />
               </div>
             }
           })}
         </Slider>
-        <div className="carousel-thumbnails">
-            <Slider
-            asNavFor = {navLarge}
-            ref={slider => (slider2 = slider)}
-            slidesToShow={images.length}
-            variableWidth ={true}
-            swipeToSlide={true}
-            focusOnSelect={true}
-            {...settings2}
-            >
-            {images.map((obj, idx) => {
-                if (obj.type && obj.type.id == 1) {
-                return <div key={idx} style={{width : 130}}>
-                    <img src={obj.thumb_image} />
-                </div>
-                } else {
-                return <div className="videoimg" key={idx} style={{width : 130}}>
-                    <img src={obj.thumb_image} />
-                </div>
-                }
-            })}
-            </Slider>
-        </div>
       </div>
+    </div>
   );
 
 }
