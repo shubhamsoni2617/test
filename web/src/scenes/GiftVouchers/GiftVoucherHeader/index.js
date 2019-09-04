f5b29ed48ac707fe576096a2bac5b321f781acdb
mohitbansal <mohit.bansal@ranosys.com>
2019-09-04 12:39:40 +0530

    gift voucher responsivness

--- web/src/scenes/GiftVouchers/GiftVoucherHeader/index.js
+++ web/src/scenes/GiftVouchers/GiftVoucherHeader/index.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./style.scss";
-import happygirl from "../../../assets/images/happy-girl.png";
+import happygirldesktop from "../../../assets/images/happy-girl-desktop.png";
+import happygirlmobile from "../../../assets/images/happy-girl.png";
 import arrow from "../../../assets/images/buy-arrow.png";
 
 const giftVoucherHeader = ({ bannerDescription }) => {
   return (
-    <div className="giftvoucherheader">
-      <div className="giftvoucherheader-wrapper">
-        <div className="container-fluid">
-          <div className="giftvoucherheader-banner">
-            <div className="banner-desc">
-              <div className="banner-title"
-                dangerouslySetInnerHTML={{
-                  __html: bannerDescription
-                }}
-              />
-              <a className="buy-now" href="/">
-                <span>BUY NOW</span>
-              </a>
-            </div>
-            <div className="banner-image">
-              <img src={happygirl} alt="HappyGirl" className="img-fluid" />
-            </div>
+    <div className="giftvoucherheader">      
+      <div className="container-fluid">
+        <div className="giftvoucherheader-banner">
+          <div className="banner-desc">
+            <div className="banner-title"
+              dangerouslySetInnerHTML={{
+                __html: bannerDescription
+              }}
+            />
+            <a className="buy-now" href="/">
+              <span>BUY NOW</span>
+            </a>
           </div>
+          {/* <div className="banner-image">
+            <img src={happygirldesktop} alt="HappyGirl" className="img-fluid desktop-banner-img" />
+            <img src={happygirlmobile} alt="HappyGirl" className="img-fluid mobile-banner-img" />
+          </div> */}
         </div>
       </div>
     </div>
