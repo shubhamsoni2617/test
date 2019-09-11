bdcf46713f9174386e19a592deeb2f0861f8595a
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-09-11 17:54:44 +0530

    facebook quote removed

--- web/src/shared/components/SocialShare/index.js
+++ web/src/shared/components/SocialShare/index.js
@@ -1,40 +1,37 @@
 import React from 'react';
 
 import {
-    FacebookShareButton,FacebookIcon,
-    TelegramShareButton, TelegramIcon,
-    EmailShareButton, EmailIcon
-  } from 'react-share';
+  FacebookShareButton, FacebookIcon,
+  TelegramShareButton, TelegramIcon,
+  EmailShareButton, EmailIcon
+} from 'react-share';
 
 
 
-  const SocialShare = (props) => {
+const SocialShare = (props) => {
 
-        const { shareUrl ,showSocialShare } = props;
-        return (
-            <div className={`social-share ${showSocialShare ? 'share-visible' :''}`}>
-                <div className="social-share-wrapper">
-                    <FacebookShareButton
-                        url={shareUrl}
-                        quote='Facebook'>
-                        <FacebookIcon title="Facebook" size={32}
-                        round />
-                    </FacebookShareButton>
-                    <TelegramShareButton
-                            url={shareUrl}
-                            quote='Telegram'>
-                            <TelegramIcon size={32}
-                            round />
-                    </TelegramShareButton>
-                    <EmailShareButton
-                            url={shareUrl}
-                            quote='Telegram'>
-                            <EmailIcon size={32}
-                            round />
-                    </EmailShareButton>
-               </div>
-            </div>
-        )
-  }
+  const { shareUrl, showSocialShare } = props;
+  return (
+    <div className={`social-share ${showSocialShare ? 'share-visible' : ''}`}>
+      <div className="social-share-wrapper">
+        <FacebookShareButton
+          url={shareUrl}>
+          <FacebookIcon title="Facebook" size={32}
+            round />
+        </FacebookShareButton>
+        <TelegramShareButton
+          url={shareUrl}>
+          <TelegramIcon size={32}
+            round />
+        </TelegramShareButton>
+        <EmailShareButton
+          url={shareUrl}>
+          <EmailIcon size={32}
+            round />
+        </EmailShareButton>
+      </div>
+    </div>
+  )
+}
 
-  export default SocialShare;
+export default SocialShare;
