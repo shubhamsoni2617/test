4b4e78a04bb69f60462a253d5ba08b36c410cd60
ekta.jain <ekta.jain@ranosys.com>
2019-09-16 12:14:33 +0530

    bts issues resolved

--- web/src/scenes/GiftVouchers/GiftVoucherHeader/index.js
+++ web/src/scenes/GiftVouchers/GiftVoucherHeader/index.js
@@ -1,28 +1,22 @@
-import React from "react";
-import "./style.scss";
-import happygirldesktop from "../../../assets/images/happy-girl-desktop.png";
-import happygirlmobile from "../../../assets/images/happy-girl.png";
-import arrow from "../../../assets/images/buy-arrow.png";
+import React from 'react';
+import './style.scss';
 
-const giftVoucherHeader = ({ bannerDescription }) => {
+const giftVoucherHeader = ({ bannerDescription, bannerUrl }) => {
   return (
-    <div className="giftvoucherheader">      
+    <div className="giftvoucherheader">
       <div className="container-fluid">
         <div className="giftvoucherheader-banner">
           <div className="banner-desc">
-            <div className="banner-title"
+            <div
+              className="banner-title"
               dangerouslySetInnerHTML={{
                 __html: bannerDescription
               }}
             />
-            <a className="buy-now" href="/">
+            <a className="buy-now" href={bannerUrl}>
               <span>BUY NOW</span>
             </a>
           </div>
-          {/* <div className="banner-image">
-            <img src={happygirldesktop} alt="HappyGirl" className="img-fluid desktop-banner-img" />
-            <img src={happygirlmobile} alt="HappyGirl" className="img-fluid mobile-banner-img" />
-          </div> */}
         </div>
       </div>
     </div>
