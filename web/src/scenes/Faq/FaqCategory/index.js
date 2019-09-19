9fa8b7749f537a012df6ad14b85e4d5e7a9642f0
mohitbansal <mohit.bansal@ranosys.com>
2019-09-19 16:26:39 +0530

    faq page design with responsive

--- web/src/scenes/Faq/FaqCategory/index.js
+++ web/src/scenes/Faq/FaqCategory/index.js
@@ -6,8 +6,9 @@ import FaqContent from "../FaqContent";
 const FaqCategory = props => {
   return (
     <Fragment>
-      {!props.urlExist ? <h2>Please, select appropriate category</h2> : null}
-      <ul>
+      {/* {!props.urlExist ? <h2>Please, select appropriate category</h2> : null} */}
+      <ul className="faq-listing">
+        <li><span className="trending-qus">Trending Question</span></li>
         {props.categories.map(category => {
           return (
             <li key={category.id}>
@@ -23,12 +24,14 @@ const FaqCategory = props => {
               </Link>
               {props.categoryId === category.id ? (
                 <Fragment>
-                  <h2>{props.categoryName}</h2>
-                  <FaqContent
-                    {...props}
-                    data={props.faqContentData}
-                    categoryId={category.id}
-                  />
+                  <div className="faq-ans-wrapper">
+                    <h2>{props.categoryName}</h2>
+                    <FaqContent
+                      {...props}
+                      data={props.faqContentData}
+                      categoryId={category.id}
+                    />
+                  </div>
                 </Fragment>
               ) : null}
             </li>
