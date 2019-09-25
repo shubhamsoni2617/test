ac9fc9f599883838a35d8a4f321ffaf8081c5fde
mohitbansal <mohit.bansal@ranosys.com>
2019-09-25 12:47:52 +0530

    api partners html design

--- web/src/scenes/ApiPartner/ApiBanner/index.js
+++ web/src/scenes/ApiPartner/ApiBanner/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './style.scss';
+import apibanner from '../../../assets/images/api-partners-banner.png'
 
 const ApiBanner = ({
 
 }) => {
     return (
-        <div>
-
+    <div className="apipartnerheader">
+        <div className="container">
+          <div className="apipartnerheader-banner">
+            <div className="banner-desc">
+                <p>API Partners</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum leo justo, vel placerat libero ornare at. Pellentesque sed dapibus felis. Integer quis dictum ante, in molestie orci. Mauris eget justo fringilla, ornare sem a, congue diam. Suspendisse sed erat mollis, mattis nisi ac, lacinia tortor.</p>
+                <div className="banner-title"/>
+            </div>
+            <div className="banner-image">
+                <img src={apibanner} alt="api-partners" />
+            </div>
+          </div>
         </div>
+    </div>
     )
 }
 export default ApiBanner;
--- web/src/scenes/ApiPartner/Partners/index.js
+++ web/src/scenes/ApiPartner/Partners/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './style.scss';
+import codpartner from '../../../assets/images/cod.png';
+import showpartner from '../../../assets/images/show.png';
+import galaxypartner from '../../../assets/images/galaxy-macau-vector-logo.png';
 
 const Partners = ({
 
 }) => {
     return (
-        <div>
-
+       <div className="work-with-partners">
+           <div className="container">
+           <h2>View some of the partners that we work with:</h2>
+            <ul className="partners-list">
+                    <li><a href=""><img src={codpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={showpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={galaxypartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={codpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={showpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={galaxypartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={codpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={showpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={galaxypartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={codpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={showpartner} alt="api-parrtner-cod" /></a></li>
+                    <li><a href=""><img src={galaxypartner} alt="api-parrtner-cod" /></a></li>
+                </ul>
+            </div>
         </div>
     )
 }
