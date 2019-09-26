41736c0aaa89d1e540069ec2a7bf334c31d146bb
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-09-26 12:48:52 +0530

    seperate component added

--- web/src/shared/components/ContactUs/index.js
+++ web/src/shared/components/ContactUs/index.js
@@ -1,16 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import ContactUsService from '../../services/ContactUsService';
+import { useCustomWidth } from '../CustomHooks';
+import Constants from '../../constants';
 
-const ContactUs = ({
-  name,
-  email,
-  phone,
-  message,
-  errMsg,
-  handleChange,
-  handleSubmit
-}) => {
+const ContactUs = props => {
+  const [width] = useCustomWidth();
+
+  const [enquiry, setEnquiry] = useState('Select an Enquiry');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+
+  useEffect(() => {
+    fetchEnquiry = () => {
+      ContactUsService.getEnquiry()
+        .then(res => {
+          if (res && res.data) {
+            setEnquiry(res.data.data);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (enquiry && name && email && phone && message) {
+      const data = {
+        category: Number(enquiry),
+        name: name,
+        email: email,
+        contact_number: phone,
+        message: message,
+        source_from: width > Constants.MOBILE_BREAK_POINT ? 1 : 3
+      };
+      submitForm(data);
+    }
+  };
+
+  const submitForm = data => {
+    ContactUsService.formSubmission(data)
+      .then(res => {
+        if (res && res.data) {
+          setSubmitResponse(res.data[0]);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    if (name === 'phone') {
+      const allowOnlyNum = /^[0-9\b]+$/;
+      if (value === '' || allowOnlyNum.test(value)) {
+        setPhone(value);
+      }
+    } else {
+      switch (name) {
+        case 'enquiry':
+          setEnquiry(value);
+          break;
+        case 'name':
+          setName(value);
+          break;
+        case 'email':
+          setEmail(value);
+          break;
+        case 'message':
+          setMessage(value);
+          break;
+        default:
+          return;
+      }
+    }
+  };
+
   return (
     <div className="contact-us banner-overlay">
       <h1 className="text-center contact-us-header">Contact Us</h1>
@@ -42,9 +114,27 @@ const ContactUs = ({
               <h3 className="heading-text">Corporate Enquiries</h3>
               <h5 className="text-success"></h5>
               <form onSubmit={e => handleSubmit(e)}>
-                <div className="form-group">
+                {/* <div className="form-group">
                   <select name="enquiry" className="form-control">
                     <option>Request Type*</option>
+                  </select>
+                </div> */}
+                <div className="form-group">
+                  <select
+                    name="enquiry"
+                    className="form-control"
+                    onChange={handleChange}
+                    value={enquiry}
+                  >
+                    <option>Select an Enquiry</option>
+                    {enquiryCategory &&
+                      enquiryCategory.map(enq => {
+                        return (
+                          <option key={enq.id} value={enq.id}>
+                            {enq.name}
+                          </option>
+                        );
+                      })}
                   </select>
                 </div>
                 <div className="form-group">
