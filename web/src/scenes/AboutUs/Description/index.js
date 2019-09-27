7d08f2cb3700123cf062c276aa3a4eb086c7b200
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-09-27 17:26:39 +0530

    API implemented

--- web/src/scenes/AboutUs/Description/index.js
+++ web/src/scenes/AboutUs/Description/index.js
@@ -1,41 +1,50 @@
 import React from 'react';
 import './style.scss';
-
 import mission from '../../../assets/images/our-mission.png';
 import history from '../../../assets/images/our-history.png';
 import promise from '../../../assets/images/our-promise.png';
 
-const Description = ({
-
-}) => {
+const Description = ({ content }) => {
   return (
     <div className="aboutus-description-wrapper">
       <div className="container">
         <section className="our-mission">
-          <div class="about-desc-img">
+          <div className="about-desc-img">
             <img src={mission} alt="our-mission" />
           </div>
           <div className="about-desc-content">
-            <h3>Our Mission</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie.</p>
+            <h3>{content && content[0].title}</h3>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: content && content[0].description
+              }}
+            ></p>
           </div>
         </section>
         <section className="our-history">
           <div className="about-desc-content">
-            <h3>Our History</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie.</p>
+            <h3>{content && content[1].title}</h3>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: content && content[1].description
+              }}
+            ></p>
           </div>
-          <div class="about-desc-img">
+          <div className="about-desc-img">
             <img src={history} alt="our-history" />
           </div>
         </section>
         <section className="our-promise">
-          <div class="about-desc-img">
+          <div className="about-desc-img">
             <img src={promise} alt="our-mission" />
           </div>
           <div className="about-desc-content">
-            <h3>Our Promise</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor nibh finibus et. Aenean eu enim justo. Vestibulum aliquam hendrerit molestie.</p>
+            <h3>{content && content[2].title}</h3>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: content && content[2].description
+              }}
+            ></p>
           </div>
         </section>
       </div>
