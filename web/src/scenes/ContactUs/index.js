980aa62b80d1bdb780cd3764ea2f99baa1ee570e
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-09-27 12:29:12 +0530

    contact component added

--- web/src/scenes/ContactUs/index.js
+++ web/src/scenes/ContactUs/index.js
@@ -9,57 +9,29 @@ class ContactUs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      enquiryCategory: [],
-      contactDetail: {},
-      category: ""
+      category: ''
     };
   }
 
   componentDidMount() {
     window.scrollTo(0, 0);
-    this.fetchEnquiry();
-    this.fetchContactDetail()
   }
 
-  fetchEnquiry = () => {
-    ContactUsService.getEnquiry()
-      .then((res) => {
-        if (res && res.data) {
-          this.setState({ enquiryCategory: res.data.data })
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  fetchContactDetail = () => {
-    ContactUsService.getContactDetail()
-      .then((res) => {
-        if (res && res.data) {
-          this.setState({ contactDetail: res.data.data });
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  sendCategoryToFaqs = (category) => {
-    this.setState({ category: category })
-  }
+  sendCategoryToFaqs = category => {
+    this.setState({ category: category });
+  };
 
   render() {
-    const { enquiryCategory, contactDetail,category } = this.state;
+    const { category } = this.state;
 
     return (
       <div className="contact-us banner-overlay">
         <h1 className="text-center contact-us-header">Contact Us</h1>
         <div className="container">
           <div className="contact-us-block">
-            <CustomerEnquiry enquiryCategory={enquiryCategory} sendCategoryToFaqs={this.sendCategoryToFaqs} />
+            <CustomerEnquiry sendCategoryToFaqs={this.sendCategoryToFaqs} />
             <div className="contact-find-us">
-              <FindUs contactDetail={contactDetail} />
+              <FindUs />
             </div>
           </div>
           <Faqs category={category} />
