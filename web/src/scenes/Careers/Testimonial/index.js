aee145a7d849917766cfc73b5822d2981eec6abf
Mayank Gupta <mayank.gupta@ranosys.com>
2019-10-17 16:41:53 +0530

    Worked on career page

--- web/src/scenes/Careers/Testimonial/index.js
+++ web/src/scenes/Careers/Testimonial/index.js
@@ -8,53 +8,53 @@ const Testimonials = ({ testimonial, testimonialErr }) => {
     focusOnSelect: false,
     className: 'center',
     centerMode: true,
-    infinite: false,
-    centerPadding: '300px',
+    infinite: true,
+    centerPadding: '250px',
     slidesToShow: 1,
-    initialSlide: 1,
     speed: 500,
     responsive: [
       {
-        breakpoint: 976,
+        breakpoint: 1024,
         settings: {
-          centerMode: true,
+          centerMode: false,
+          centerPadding: '0px'
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          centerMode: false,
           centerPadding: '0px'
         }
       }
-      // {
-      //   breakpoint: 600,
-      //   settings: {
-      //     centerMode: false,
-      //     centerPadding: '0px'
-      //   }
-      // }
     ]
   };
   return (
-    <div className="testimonials-wrapper">
-      <div className="testimonials">
-        <h3 className="text-center">Testimonials</h3>
-        <div className="testimonials-content">
-          <ul className="testimonials-slider">
-            <Slider {...settings}>
-              {testimonial &&
-                testimonial.testimonials &&
-                testimonial.testimonials.map((elem, index) => {
-                  return (
-                    <li key={index}>
-                      <div className="message">{elem.message}</div>
-                      <div className="name text-center">
-                        <h5>{elem.name}</h5>
-                      </div>
-                      <img src={elem.image} height="200" width="100%" />
-                    </li>
-                  );
-                })}
-            </Slider>
-          </ul>
-        </div>
+  <div className="testimonials-wrapper">
+    <div className="testimonials">
+      <h3 className="text-center">Testimonials</h3>
+      <div className="testimonials-content">
+      <ul className="testimonials-slider">
+      <Slider {...settings}>
+        {testimonial &&
+          testimonial.testimonials &&
+          testimonial.testimonials.map((elem, index) => {
+            return (
+              <li key={index}>
+                <div className="message">{elem.message}</div>
+                <div className="name text-center">
+                  <h5>{elem.name}</h5>
+                </div>
+                <img src={elem.image} height="200" width="100%" />
+              </li>
+            );
+          })}
+      </Slider>
+      </ul>
       </div>
     </div>
+    </div>
+
   );
 };
 
