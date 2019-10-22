62f4f911f38858d47d18bd3262faa972e8a91795
mohitbansal <mohit.bansal@ranosys.com>
2019-10-22 19:17:30 +0530

    Home page new sections design

--- web/src/scenes/Home/CustomSection/CustomSectionThree/index.js
+++ web/src/scenes/Home/CustomSection/CustomSectionThree/index.js
@@ -47,75 +47,75 @@ const CustomSectionThree = ({ customSectionThree }) => {
           type="LIST"
         />
       ) : (
-        <section className="video-gallery">
-          <div className="container-fluid">
-            <div className="section-top-wrapper">
-              <h2>
-                {customSectionThree &&
-                  customSectionThree[0] &&
-                  customSectionThree[0].title}
-              </h2>
-              <div className="carousel-dots">
-                <a href="/events">
-                  See all{' '}
-                  <img
-                    src="assets/images/right-arrow.svg"
-                    className="img-fluid"
-                    alt="arrow"
-                  />
-                </a>
+          <section className="video-gallery">
+            <div className="container-fluid">
+              <div className="section-top-wrapper">
+                <h2>
+                  {customSectionThree &&
+                    customSectionThree[0] &&
+                    customSectionThree[0].title}
+                </h2>
+                <div className="carousel-dots">
+                  <a href="/events">
+                    See all{' '}
+                    <img
+                      src="assets/images/right-arrow.svg"
+                      className="img-fluid"
+                      alt="arrow"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="video-gallery-wrapper">
+                <div className="video-only-section">
+                  <ReactPlayer
+                    width="100%"
+                    height="100%"
+                    controls
+                    pip={pip}
+                    muted={muted}
+                    url={url && url}
+                    playing={playing}
+                    // light={light}
+                    volume={volume}
+                  />{' '}
+                  <h3>Celebrate 2016 - Teaser: Tickets at SISTIC from 9 Nov</h3>
+                </div>
+                <div className="video-subwrapper">
+                  {customSectionThree &&
+                    customSectionThree
+                      .slice(1, customSectionThree.length)
+                      .map((vdo, index) => {
+                        return (
+                          <div
+                            className="video-item-image"
+                            key={index}
+                            onClick={() => {
+                              setUrl(vdo.video_url);
+                              setControls(true);
+                              setPip(true);
+                              setLight(false);
+                              setMuted(false);
+                              setVolume(0.5);
+                            }}
+                          >
+                            <span className="video-subwrapper-image">
+                              <img
+                                src={vdo.video_thumb}
+                                alt=""
+                                className="img-fluid"
+                              />
+                            </span>
+                            <a className="video-subwrapper-text">{vdo.title}</a>
+                            {/* <a >[HD] Universal Studios Singapore Tour - Universal Studios Theme Park</a> */}
+                          </div>
+                        );
+                      })}
+                </div>
               </div>
             </div>
-            <div className="video-gallery-wrapper">
-              <div className="video-only-section">
-                <ReactPlayer
-                  width="100%"
-                  height="100%"
-                  controls
-                  pip={pip}
-                  muted={muted}
-                  url={url && url}
-                  playing={playing}
-                  // light={light}
-                  volume={volume}
-                />{' '}
-                <h3>Celebrate 2016 - Teaser: Tickets at SISTIC from 9 Nov</h3>
-              </div>
-              <div className="video-subwrapper">
-                {customSectionThree &&
-                  customSectionThree
-                    .slice(1, customSectionThree.length)
-                    .map((vdo, index) => {
-                      return (
-                        <div
-                          className="video-item-image"
-                          key={index}
-                          onClick={() => {
-                            setUrl(vdo.video_url);
-                            setControls(true);
-                            setPip(true);
-                            setLight(false);
-                            setMuted(false);
-                            setVolume(0.5);
-                          }}
-                        >
-                          <span className="video-subwrapper-image">
-                            <img
-                              src={vdo.video_thumb}
-                              alt=""
-                              className="img-fluid"
-                            />
-                          </span>
-                          <a className="video-subwrapper-text">{vdo.title}</a>
-                          {/* <a >[HD] Universal Studios Singapore Tour - Universal Studios Theme Park</a> */}
-                        </div>
-                      );
-                    })}
-              </div>
-            </div>
-          </div>
-        </section>
-      )}
+          </section>
+        )}
     </div>
   );
 };
--- web/src/scenes/Home/CustomSection/CustomSectionTwo/index.js
+++ web/src/scenes/Home/CustomSection/CustomSectionTwo/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import './style.scss';
 import Image from '../../../../shared/components/Image';
 import ShimmerEffect from '../../../../shared/components/ShimmerEffect';
-import pridge from '../../../../assets/images/pride.jpg';
+import royal from '../../../../assets/images/royal.jpg';
 import royalSubimg from '../../../../assets/images/royal-img.png';
 
 const CustomSectioTwo = ({ customSectionTwo, loading }) => {
@@ -16,66 +16,68 @@ const CustomSectioTwo = ({ customSectionTwo, loading }) => {
   ) : (
       <section className="royal-wrapper">
         <div className="container-fluid">
-          <div className="section-top-wrapper">
-            <h2>Royals</h2>
-            <div className="carousel-dots">
-              <a href="/events">
-                More from Royals {' '}
-                <img
-                  src="assets/images/right-arrow.svg"
-                  className="img-fluid"
-                  alt="arrow"
-                />
-              </a>
+          <div className="royal-side-padding">
+            <div className="section-top-wrapper">
+              <h2>Royals</h2>
+              <div className="carousel-dots">
+                <a href="/events">
+                  More from Royals {' '}
+                  <img
+                    src="assets/images/right-arrow.svg"
+                    className="img-fluid"
+                    alt="arrow"
+                  />
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="royal-items-wrapper">
-            <div className="royal-leftsection">
-              <a
-                href={
-                  customSectionTwo &&
-                  customSectionTwo[0] &&
-                  customSectionTwo[0].navigation_link
-                }
-                target="_blank"
-              >
-                <div>
-                  {/* {customSectionTwo && customSectionTwo[0] && (
+            <div className="royal-items-wrapper">
+              <div className="royal-leftsection">
+                <a
+                  href={
+                    customSectionTwo &&
+                    customSectionTwo[0] &&
+                    customSectionTwo[0].navigation_link
+                  }
+                  target="_blank"
+                >
+                  <div>
+                    {/* {customSectionTwo && customSectionTwo[0] && (
                     <Image
                       src={customSectionTwo[0].full_image}
                       alt={customSectionTwo[0].alt}
                       type="BigBanner"
                     />
                   )} */}
-                  <img src={pridge} alt="pride" className="img-fluid" />
-                </div>
-              </a>
-            </div>
-            <div className="royal-rightsection">
-              <h3>{customSectionTwo && customSectionTwo[0] && customSectionTwo[0].title}</h3>
-              <p>10 Jan 2019</p>
-              <div className="royal-items">
-                {customSectionTwo &&
-                  customSectionTwo.slice(1, customSectionTwo.length).map((elem, i) => {
-                    return (
-                      <div key={i} className="item-wrapper">
-                        <a className="item-img" href={elem.navigation_link} target="_blank">
-                          {/* {elem && elem.full_image && (
+                    <img src={royal} alt="pride" className="img-fluid" />
+                  </div>
+                </a>
+              </div>
+              <div className="royal-rightsection">
+                <h3>{customSectionTwo && customSectionTwo[0] && customSectionTwo[0].title}</h3>
+                <p>10 Jan 2019</p>
+                <div className="royal-items">
+                  {customSectionTwo &&
+                    customSectionTwo.slice(1, customSectionTwo.length).map((elem, i) => {
+                      return (
+                        <div key={i} className="item-wrapper">
+                          <a className="item-img" href={elem.navigation_link} target="_blank">
+                            {/* {elem && elem.full_image && (
                             <Image
                               src={elem.full_image}
                               alt={elem.alt}
                               type="Horizontal"
                             />
                           )} */}
-                          <img src={royalSubimg} alt="pride" className="img-fluid" />
-                        </a>
-                        <div className="royal-item-content">
-                          <h3>{elem.title}</h3>
-                          <p>24 May 2019</p>
+                            <img src={royalSubimg} alt="pride" className="img-fluid" />
+                          </a>
+                          <div className="royal-item-content">
+                            <h3>{elem.title}</h3>
+                            <p>24 May 2019</p>
+                          </div>
                         </div>
-                      </div>
-                    );
-                  })}
+                      );
+                    })}
+                </div>
               </div>
             </div>
           </div>
