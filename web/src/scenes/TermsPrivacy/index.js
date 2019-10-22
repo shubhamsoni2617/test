b60fd4a495d69fa97a140a6ca671dd5e93582f66
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-10-22 11:34:18 +0530

    second banner disabled

--- web/src/scenes/TermsPrivacy/index.js
+++ web/src/scenes/TermsPrivacy/index.js
@@ -7,11 +7,23 @@ import ShimmerEffect from '../../shared/components/ShimmerEffect';
 
 const TermsPrivacy = props => {
   const [termsprivacy, setTermsPrivacy] = useState(null);
+  const [tabTitle, setTabTitle] = useState('');
+  const [tabDescription, setTabDescription] = useState('');
+  const [isShimmerOn, setIsShimmerOn] = useState(false);
+
+  let termsPrivacyArr;
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     fetchTermsConditions();
   }, []);
+
+  useEffect(() => {
+    setIsShimmerOn(true);
+    setTimeout(() => {
+      setIsShimmerOn(false);
+    }, 1000);
+  }, [tabTitle]);
 
   const fetchTermsConditions = () => {
     const params = {
@@ -19,18 +31,33 @@ const TermsPrivacy = props => {
     };
     TermsAndPrivacyService.getTermsAndPrivacyService(params)
       .then(res => {
+        let data = res.data.data;
         setTimeout(() => {
-          setTermsPrivacy(res.data.data);
-        }, 700);
+          setTermsPrivacy(data);
+          setTabTitle(data.terms_cond.title);
+          setTabDescription(data.terms_cond.description);
+        }, 1000);
       })
       .catch(err => {
         console.log(err);
       });
   };
+
+  const handleActiveTab = (title, description) => {
+    setTabTitle(title);
+    setTabDescription(description);
+  };
+
+  if (termsprivacy) {
+    termsPrivacyArr = Object.keys(termsprivacy).map(function(key) {
+      return termsprivacy[key];
+    });
+  }
+
   return (
     <Fragment>
       <section className="terms-privacy-wrapper">
-        <div className="banner-wrapper">
+        {/* <div className="banner-wrapper">
           {props.cmsPageType === 1 ? (
             <img src={termsBanner} className="img-fluid" alt="page-banner" />
           ) : (
@@ -39,11 +66,61 @@ const TermsPrivacy = props => {
 
           <div className="banner-overlay">
             {props.cmsPageType === 1 && termsprivacy
-              ? termsprivacy && <h1>{termsprivacy.terms_cond.title}</h1>
+              ? termsprivacy && <h1>{tabTitle}</h1>
               : termsprivacy && <h1>{termsprivacy.privacy_policy.title}</h1>}
           </div>
+        </div> */}
+
+        <div className="banner-wrapper">
+          <img src={termsBanner} className="img-fluid" alt="page-banner" />
+          <div className="banner-overlay">
+            <h1>{tabTitle}</h1>
+          </div>
         </div>
-        {termsprivacy && props.cmsPageType === 1 ? (
+
+        <div className="promotions-nav container">
+          <ul className="nav nav-tabs" id="nav-tab" role="tablist">
+            {termsPrivacyArr &&
+              termsPrivacyArr.map((category, i) => {
+                return (
+                  <li key={category.title}>
+                    <a
+                      className={
+                        tabTitle === category.title
+                          ? 'nav-item nav-link active'
+                          : 'nav-item nav-link'
+                      }
+                      onClick={() =>
+                        handleActiveTab(category.title, category.description)
+                      }
+                    >
+                      {category.title}
+                    </a>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <div className="terms-privacy-body">
+          {isShimmerOn ? (
+            <div className="container">
+              <ShimmerEffect
+                propCls="shm_col-xs-6 col-md-12"
+                height={200}
+                count={2}
+                type="TILE"
+              />
+            </div>
+          ) : (
+            <div
+              className="container"
+              dangerouslySetInnerHTML={{
+                __html: tabDescription
+              }}
+            />
+          )}
+        </div>
+        {/* {termsprivacy && props.cmsPageType === 1 ? (
           <div className="terms-privacy-body">
             <div
               className="container"
@@ -68,7 +145,7 @@ const TermsPrivacy = props => {
             count={3}
             type="TILE"
           />
-        )}
+        )} */}
       </section>
     </Fragment>
   );
