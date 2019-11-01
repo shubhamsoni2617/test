6d11ac400ba0e1cbe2b8d36d5c03c73268cb4e11
Shubham.soni <Shubham.soni@ranosys.com>
2019-11-01 21:26:49 +0530

    advertise done

--- web/src/scenes/Advertise/index.js
+++ web/src/scenes/Advertise/index.js
@@ -67,18 +67,16 @@ const Advertise = props => {
       <section>
         <div className="container">
           <div className="why-sistic">
-            <div className="why-sistic-head">
-              <h2>Why SISTIC?</h2>
-              <p>
-                The SISTIC audience is a perfect fit for show organisers and
-                companies offering lifestyle products. For advertising rates and
-                bookings of your preferred medium
-              </p>
-              <span>
-                Reach us via email at{' '}
-                <a herf="#">sisticservices@sistic.com.sg</a>
-              </span>
-            </div>
+            {advertiseData && (
+              <div className="why-sistic-head">
+                <h2>{advertiseData.content[0].title}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: advertiseData.content[0].description
+                  }}
+                />
+              </div>
+            )}
             <div className="why-sistic-tab">
               <h2>Our Advertising channels</h2>
               <div className="sistic-tab-button">
@@ -94,16 +92,16 @@ const Advertise = props => {
 
       <section>
         <div className="website-wrapper">
-          <div className="website-content">
-            <h2>Website</h2>
-            <p>
-              Advertising banners on the Homepage, Events Calendar and spaces
-              across the website are available in a myriad of sizes and
-              placements to suit your needs. With monthly page views of more
-              than 4 million on average and growing, these spaces are prime real
-              estate to increase visibility and awareness of your event.
-            </p>
-          </div>
+          {advertiseData && (
+            <div className="website-content">
+              <h2>{advertiseData.content[1].title}</h2>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: advertiseData.content[1].description
+                }}
+              />
+            </div>
+          )}
           <div className="website-image text-right">
             <img src={img1} alt="websiter-image" className="img-fluid" />
           </div>
@@ -114,15 +112,17 @@ const Advertise = props => {
         <div className="container-fluid">
           <div className="app-wrapper flex-row-reverse">
             <div className="app-content text-right pl-5">
-              <h2>Mobile App</h2>
-              <p>
-                Access live entertainment at one’s fingertips – The SISTIC
-                mobile app has garnered over 210k downloads since it’s revamp in
-                2015 and records an average of 21,000 users each month.
-                Advertise your show on the Home Screen Carousel Banner for all
-                app users to see up front and centre from the moment they launch
-                their app.
-              </p>
+              {advertiseData && (
+                <>
+                  <h2>{advertiseData.content[2].title}</h2>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: advertiseData.content[2].description
+                    }}
+                  />
+                </>
+              )}
+
               <div className="download-option app-option">
                 <a>
                   <img src={apple} className="ios" alt="" />
@@ -153,16 +153,16 @@ const Advertise = props => {
         <div className="container-fluid">
           <div className="app-wrapper">
             <div className="app-content pr-5">
-              <h2>SISTIC Buzz</h2>
-              <p>
-                Over 500,000 of our subscribers stays on top of what’s hot
-                through a fortnightly entertainment feed via SISTIC Buzz, our
-                free in-house e-newsletter. Besides featuring the latest events
-                on sale through SISTIC, SISTIC Buzz also rewards our readers
-                with special discounts and exciting contests. Show and event
-                promoters gets an extra push for their events via advertising in
-                the newsletter as well.
-              </p>
+              {advertiseData && (
+                <>
+                  <h2>{advertiseData.content[3].title}</h2>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: advertiseData.content[3].description
+                    }}
+                  />
+                </>
+              )}
               <div className="stay-connect buzz-connect">
                 <h3>Subscribe to receive our newsletter</h3>
                 <div className="input-group">
@@ -192,15 +192,16 @@ const Advertise = props => {
         <div className="container-fluid">
           <div className="app-wrapper flex-row-reverse">
             <div className="app-content text-right pl-5">
-              <h2>Social Media</h2>
-              <p>
-                Want first dibs on upcoming SISTIC events? Then let’s get
-                social! Like us on Facebook and follow us on Instagram for daily
-                updates on the hottest events in town, our latest giveaways and
-                exclusive behind the scenes content. Whether you are a concert
-                junkie or a theatre fan, be sure to tag us and share your live
-                experiences with us.
-              </p>
+              {advertiseData && (
+                <>
+                  <h2>{advertiseData.content[4].title}</h2>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: advertiseData.content[4].description
+                    }}
+                  />
+                </>
+              )}
               <div className="download-option social-option">
                 <a>
                   <img src={fb} className="fb" alt="" />
