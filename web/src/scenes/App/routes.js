8cafe3de2c08773777700989302ead3223e01c19
mohitbansal <mohit.bansal@ranosys.com>
2019-11-04 18:11:11 +0530

    sell tickets with us page design

--- web/src/scenes/App/routes.js
+++ web/src/scenes/App/routes.js
@@ -19,6 +19,7 @@ import ApiPartner from '../ApiPartner';
 import Search from '../Home/Search';
 import SystemLicensing from '../SystemLicensing';
 import Advertise from '../Advertise';
+import SellTicketsWithUs from '../SellTicketsWithUs';
 
 const routes = [
   {
@@ -131,6 +132,11 @@ const routes = [
   {
     path: '/advertise',
     component: Advertise,
+    exact: true
+  },
+  {
+    path: '/sell-tickets-with-us',
+    component: SellTicketsWithUs,
     exact: true
   },
   {
