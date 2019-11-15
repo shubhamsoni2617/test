38e9304f46a1224c39b862dfaefc7aa5dc565b1e
mohitbansal <mohit.bansal@ranosys.com>
2019-11-15 13:48:41 +0530

    festivals page design

--- web/src/shared/components/BreadcrumbSlug/index.js
+++ web/src/shared/components/BreadcrumbSlug/index.js
@@ -3,22 +3,20 @@ import './style.scss';
 import { Link } from 'react-router-dom';
 const BreadcrumbSlug = ({ breadcrumbSlug }) => {
   return (
-    <div className="container-fluid">
-      <ul>
-        {breadcrumbSlug &&
-          breadcrumbSlug.map((link, i) => {
-            return (
-              <li key={link.title} className="breadcrumb-slug">
-                {breadcrumbSlug.length === i + 1 ? (
-                  link.title
-                ) : (
+    <ul>
+      {breadcrumbSlug &&
+        breadcrumbSlug.map((link, i) => {
+          return (
+            <li key={link.title} className="breadcrumb-slug">
+              {breadcrumbSlug.length === i + 1 ? (
+                link.title
+              ) : (
                   <Link to={link.path}>{link.title}/</Link>
                 )}
-              </li>
-            );
-          })}
-      </ul>
-    </div>
+            </li>
+          );
+        })}
+    </ul>
   );
 };
 export default BreadcrumbSlug;
