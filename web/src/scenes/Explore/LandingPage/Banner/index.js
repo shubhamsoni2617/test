9c425d8f4448b18f100cd38e2fb1a4e32b1d87f2
shubham.soni <shubham.soni@ranosys.com>
2019-11-15 20:14:43 +0530

    landing Banner

--- web/src/scenes/Explore/LandingPage/Banner/index.js
+++ web/src/scenes/Explore/LandingPage/Banner/index.js
@@ -1,28 +1,98 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './style.scss';
 import bannerImg from '../../../../assets/images/explore-banner2.png';
+import Slider from 'react-slick';
 
-const Banner = ({}) => {
+const BannerSlider = props => {
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
+  let slider1 = useRef();
+  let slider2 = useRef();
+
+  useEffect(() => {
+    setNav1(slider1);
+    setNav2(slider2);
+  }, []);
+
+  const settings = {
+    arrows: false,
+    draggable: false
+  };
+  const settingsSlider = {
+    autoplay: true
+  };
   return (
     <section className="explore-page-banner">
       <div className="banner-content">
-        <div className="banner-desc">
-          <h3>Kurios: Cabinet of Curiosities Cirque du Soleil</h3>
-          <p>
-            Cirque du Soleil arrives in Singapore with its most acclaimed
-            touring show to date called, KURIOS – Cabinet of Curiosities. Kurios
-            will premier under a new grey-and-white Big Top from 6 July 2019…
-          </p>
-        </div>
-        <div className="banner-btn">
-          <a>Read More</a>
-        </div>
+        <Slider
+          {...settings}
+          asNavFor={nav2}
+          ref={slider => (slider1 = slider)}
+        >
+          <div>
+            <h3>1</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>2</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>3</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>4</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>5</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>6</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+        </Slider>
       </div>
+
       <div className="banner-slider">
-        <img src={bannerImg} alt="explore-slider" />
+        <Slider
+          {...settingsSlider}
+          asNavFor={nav1}
+          ref={slider => (slider2 = slider)}
+          slidesToShow={5}
+          swipeToSlide={true}
+          focusOnSelect={true}
+        >
+          <div>
+            <h3>1</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>2</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>3</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>4</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>5</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+          <div>
+            <h3>6</h3>
+            <img src={bannerImg} alt="explore-slider" />
+          </div>
+        </Slider>
       </div>
     </section>
   );
 };
 
-export default Banner;
+export default BannerSlider;
