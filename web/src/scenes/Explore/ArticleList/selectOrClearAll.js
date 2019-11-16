5507371bbfa9542b966f36896b3168d494922339
shubham.soni <shubham.soni@ranosys.com>
2019-11-16 13:57:30 +0530

    listing

--- web/src/scenes/Explore/ArticleList/selectOrClearAll.js
+++ web/src/scenes/Explore/ArticleList/selectOrClearAll.js
@@ -1,6 +1,10 @@
 export default (isChecked, filterList, setFilterList, setFilteredId) => {
   const tagsUpdated = [...filterList];
+  let filteredId = [];
   tagsUpdated.forEach(tag => (tag.isChecked = isChecked));
+  if (isChecked) {
+    tagsUpdated.map(tag => filteredId.push(tag.id));
+  }
   setFilterList(tagsUpdated);
-  setFilteredId([]);
+  setFilteredId(filteredId);
 };
