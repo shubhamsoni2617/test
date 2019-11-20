f97e66c2243dd579880b44130ca66a3788676075
Jeetendra singh <jeetendra.singh@ranosys.com>
2019-11-20 18:14:05 +0530

    Working home page slider

--- web/src/scenes/Home/HomePageCarouselContainer/index.js
+++ web/src/scenes/Home/HomePageCarouselContainer/index.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import './style.scss';
 import HomeService from '../../../shared/services/HomeService';
+import Utilities from '../../../shared/utilities';
 
 const mainSliderSettings = {
     className: "center",
     centerMode: true,
     infinite: true,
     focusOnSelect: true,
-    centerPadding: "360px",
+    centerPadding: Utilities.mobilecheck() ? "60px" : "200px" ,
     slidesToShow: 1,
     speed: 500,
     arrows: true
@@ -17,11 +18,12 @@ const thumbSliderSettings = {
     centerMode: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 10,
+    slidesToShow: Utilities.mobilecheck() ? 6 : 13,
     slidesToScroll: 1,
     focusOnSelect: true,
     arrows: true
 };
+
 
 export default class HomePageCarouselContainer extends Component {
 
@@ -70,48 +72,50 @@ export default class HomePageCarouselContainer extends Component {
         }
 
         return (
-            <div>
-                {posts.length > 0 && <div>
+            <div className="banner">
+                <div className="banner-carousel">
+                {posts.length > 0 && <div className="active-banner-image">
                     <img src={posts[sliderBackgroudImage].full_image} /></div>}
+                    <Slider {...mainSliderSettings}
+                        asNavFor={this.thumbSlider}
+                        ref={slider => (this.mainSlider = slider)}
+                        afterChange={(index) => this.changeBackgroundImage(index)}
+                        className="slider-for">
 
-                <Slider {...mainSliderSettings}
-                    asNavFor={this.thumbSlider}
-                    ref={slider => (this.mainSlider = slider)}
-                    afterChange={(index) => this.changeBackgroundImage(index)}
-                    className="slider-for">
-
-                    {
-                        posts.length ?
-                            posts.map((post, key) => <div>
-                                <img onClick={sliderBackgroudImage == key ? ()=> window.open("someLink", "_blank") : ''} src={post.full_image} alt="image1" className="img1 img-responsive"></img>
-                            </div>) :
-                            null
-                    }
-                    {
-                        errorMsg ? <div>{errorMsg}</div> : null
-                    }
-
+                        {
+                            posts.length ?
+                                posts.map((post, key) => <div>
+                                    <img onClick={sliderBackgroudImage == key ? ()=> window.open("someLink", "_blank") : ''} src={post.full_image} alt="image1" className="img1 img-responsive"></img>
+                                </div>) :
+                                null
+                        }
+                        {
+                            errorMsg ? <div>{errorMsg}</div> : null
+                        }
 
 
-                </Slider>
 
-                <Slider {...thumbSliderSettings}
-                    asNavFor={this.mainSlider}
-                    ref={slider => (this.thumbSlider = slider)}
-                    focusOnSelect={true}
-                    swipe={false}
-                >
-                    {
-                        posts.length ?
-                            posts.map(post => <div>
-                                <img src={post.full_image} alt="image1" className="img img-responsive"></img>
-                            </div>) :
-                            null
-                    }
-                    {
-                        errorMsg ? <div>{errorMsg}</div> : null
-                    }
-                </Slider>
+                    </Slider>
+                </div>
+                <div className="banner-thumbnail">
+                    <Slider {...thumbSliderSettings}
+                        asNavFor={this.mainSlider}
+                        ref={slider => (this.thumbSlider = slider)}
+                        focusOnSelect={true}
+                        swipe={false}
+                    >
+                        {
+                            posts.length ?
+                                posts.map(post => <div>
+                                    <img src={post.full_image} alt="image1" className="img img-responsive"></img>
+                                </div>) :
+                                null
+                        }
+                        {
+                            errorMsg ? <div>{errorMsg}</div> : null
+                        }
+                    </Slider>
+                </div>
             </div>
         );
     }
