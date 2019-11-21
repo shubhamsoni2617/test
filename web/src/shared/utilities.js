e8ee17d9bd55bbe2b297f968e369838d1ca7e91c
dipesh.tiwari <dipesh.tiwari@ranosys.com>
2019-11-21 20:00:26 +0530

    Edge issue resolved

--- web/src/shared/utilities.js
+++ web/src/shared/utilities.js
@@ -144,7 +144,7 @@ class Utilities {
   }
 
   static updateUrl(history, state) {
-    
+
     let url;
     let searchParam = "";
     searchParam = "?";
@@ -169,6 +169,17 @@ class Utilities {
     history.push({
       search: url
     });
+  }
+
+  static browserDetect() {
+    var ua = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i),browser;
+    if (navigator.userAgent.match(/Edge/i) || navigator.userAgent.match(/Trident.*rv[ :]*11\./i)) {
+      browser = "msie";
+    }
+    else {
+      browser = ua[1].toLowerCase();
+    }
+    return browser;
   }
 }
 
