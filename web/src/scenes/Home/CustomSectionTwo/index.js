a1d9b84642a156dc3b5d1f36a46b660ee4073be6
Mayank Gupta <mayank.gupta@ranosys.com>
2019-11-22 14:48:42 +0530

    Worked on explore landing page ui

--- web/src/scenes/Home/CustomSectionTwo/index.js
+++ web/src/scenes/Home/CustomSectionTwo/index.js
@@ -51,7 +51,6 @@ const CustomSectionTwo = ({ heading, customData, isMoreFrom }) => {
   ) : (
     <section className="royal-wrapper">
       <div className="container-fluid">
-        <div className="royal-side-padding">
           <div className="section-top-wrapper">
             <h2>{heading}</h2>
             {isMoreFrom && (
@@ -122,7 +121,6 @@ const CustomSectionTwo = ({ heading, customData, isMoreFrom }) => {
               </div>
             </div>
           </div>
-        </div>
       </div>
     </section>
   );
