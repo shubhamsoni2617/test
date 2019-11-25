3f28a088348ef6c0e7d814e32f72516657fb5fce
Pawan <pawan.jangid@ranosys.com>
2019-11-25 12:09:59 +0530

    Event detail issues

--- web/src/shared/components/Modal/index.js
+++ web/src/shared/components/Modal/index.js
@@ -1,48 +1,55 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import Modal from './ModalComponent';
 import './style.scss';
-export default class ModalPopup extends React.Component {
-  render() {
-    const { handleClose, content, title, showModal, htmlContent } = this.props;
-    if (showModal && typeof window !== undefined) {
-      document.body.classList.add('fixed-body');
-    }
-    return (
-      <div>
-        <Modal
-          visible={showModal}
-          closemodal={e => {
-            if (typeof window !== undefined) {
-              document.body.classList.remove('fixed-body');
-            }
-            handleClose(e);
-          }}
-          type="fadeInDown"
-        >
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                {title}
-              </h5>
-            </div>
-            <div className="modal-body">
-              {htmlContent ? (
-                <div
-                  className="notice"
-                  dangerouslySetInnerHTML={{ __html: content }}
-                ></div>
-              ) : (
-                <div className="notice">{content}</div>
-              )}
-              {this.props.children}
-            </div>
-          </div>
-        </Modal>
-      </div>
-    );
+function ModalPopup({
+  handleClose,
+  content,
+  title,
+  showModal,
+  htmlContent,
+  children
+}) {
+  console.log('modal');
+  if (showModal && typeof window !== undefined) {
+    document.body.classList.add('fixed-body');
   }
+  return (
+    <div>
+      <Modal
+        visible={showModal}
+        closemodal={e => {
+          if (typeof window !== undefined) {
+            document.body.classList.remove('fixed-body');
+          }
+          handleClose(e);
+        }}
+        type="fadeInDown"
+      >
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              {title}
+            </h5>
+          </div>
+          <div className="modal-body">
+            {htmlContent ? (
+              <div
+                className="notice"
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></div>
+            ) : (
+              <div className="notice">{content}</div>
+            )}
+            {children}
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
 }
+
+export default memo(ModalPopup);
 
 ModalPopup.propTypes = {
   content: PropTypes.string,
