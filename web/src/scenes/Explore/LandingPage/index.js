a227d36d47145f0ca46d9cd809e118c24c670f5d
Ashish Ranjan <ashish.ranjan@ranosys.com>
2019-11-27 19:26:27 +0530

    shimmer effect added in landing page

--- web/src/scenes/Explore/LandingPage/index.js
+++ web/src/scenes/Explore/LandingPage/index.js
@@ -15,23 +15,10 @@ import ShimmerEffect from '../../../shared/components/ShimmerEffect';
 
 const Explore = () => {
   const [data, setData] = useState([]);
-  const [sectionOrders, setSectionOrders] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     scrollToTop();
-    const sectionOrders = [
-      { order: 'whatsUp' },
-      { order: 'banner' },
-      { order: 'royals' },
-      { order: 'festival' },
-      { order: 'videoGallery' },
-      { order: 'GiftCard' },
-      { order: 'Reviews' },
-      { order: 'articles' },
-      { order: 'trending' }
-    ];
-    setSectionOrders(shuffle(sectionOrders));
     setLoading(true);
     getExploreLanding();
   }, []);
@@ -53,20 +40,6 @@ const Explore = () => {
           setLoading(false);
         }
       });
-  };
-
-  const shuffle = array => {
-    var ctr = array.length,
-      temp,
-      index;
-    while (ctr > 0) {
-      index = Math.floor(Math.random() * ctr);
-      ctr--;
-      temp = array[ctr];
-      array[ctr] = array[index];
-      array[index] = temp;
-    }
-    return array;
   };
 
   const reusedShimmer = (height, count, type, propCls) => {
