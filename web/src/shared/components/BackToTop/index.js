c07a8e186aeafb9a32afd4ce371e423568683ffd
shubham.soni <shubham.soni@ranosys.com>
2019-12-13 12:58:07 +0530

    refactor

--- web/src/shared/components/BackToTop/index.js
+++ web/src/shared/components/BackToTop/index.js
@@ -1,54 +1,33 @@
-import React from 'react';
-import scrollTop from '../../../assets/images/arrow-to-top.svg';
-import PropTypes from 'prop-types';
-
-class BackToTop extends React.Component {
-  state = {
-    intervalId: 0,
-    thePosition: false
+import React, { useState, useEffect } from 'react';
+import topArrow from '../../../assets/images/arrow-to-top.svg';
+import Utilities from '../../utilities';
+const BackToTop = () => {
+  const [yOffset, setYOffset] = useState(0);
+  useEffect(() => {
+    window.addEventListener('scroll', listenToScroll);
+    return () => {
+      window.removeEventListener('scroll', listenToScroll);
+    };
+  }, []);
+  const listenToScroll = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrolled = winScroll / height;
+    setYOffset(scrolled);
   };
-
-  componentDidMount() {
-    document.addEventListener('scroll', () => {
-      if (window.scrollY > 170) {
-        this.setState({ thePosition: true });
-      } else {
-        this.setState({ thePosition: false });
-      }
-    });
+  const scrollTop = () => {
     window.scrollTo(0, 0);
-  }
-
-  onScrollStep = () => {
-    if (window.pageYOffset === 0) {
-      clearInterval(this.state.intervalId);
-    }
-    window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);
   };
-
-  scrollToTop = () => {
-    let intervalId = setInterval(this.onScrollStep, this.props.delayInMs);
-    this.setState({ intervalId: intervalId });
-  };
-
-  renderGoTopIcon = () => {
-    if (this.state.thePosition) {
-      return (
-        <span className="scroll-top" onClick={this.scrollToTop}>
-          <img src={scrollTop} alt="Scroll to top" />
-        </span>
-      );
-    }
-  };
-
-  render() {
-    return <React.Fragment>{this.renderGoTopIcon()}</React.Fragment>;
-  }
-}
-
-export default BackToTop;
-
-BackToTop.propTypes = {
-  delayInMs: PropTypes.string.isRequired,
-  scrollStepInPx: PropTypes.string.isRequired
+  return (
+    !Utilities.mobilecheck() &&
+    yOffset && (
+      <span className="scroll-top" onClick={() => scrollTop()}>
+        <img src={topArrow} alt="Scroll to top" />
+      </span>
+    )
+  );
 };
+export default BackToTop;
