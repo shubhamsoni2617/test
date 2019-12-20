c441fd42152a8ded918949964e2dae0c16e8bb40
mohitbansal <mohit.bansal@ranosys.com>
2019-12-20 20:33:04 +0530

    search listing responsive design

--- web/src/scenes/Home/Search/Card/index.js
+++ web/src/scenes/Home/Search/Card/index.js
@@ -3,6 +3,9 @@ import './style.scss';
 import Image from '../../../../shared/components/Image';
 import navigateToLink from '../../../../shared/HelperFunctions/navigateToLink';
 import './style.scss';
+import EventHeading from '../../../../shared/components/EventHeading';
+import Utilities from '../../../../shared/utilities';
+import Ellipsis from '../../../../shared/components/Ellipsis';
 
 const Card = props => {
   const { cardData } = props;
@@ -12,7 +15,7 @@ const Card = props => {
         cardData.type === 'event' || cardData.type === 'attractions'
           ? ``
           : `full-desc`
-      }`}
+        }`}
       onClick={() =>
         navigateToLink(
           props.history,
@@ -36,9 +39,27 @@ const Card = props => {
             <h6>{'attraction'.toUpperCase()}</h6>
           )}
           <div className="item-title">
-            <h5>{cardData.title}</h5>
-            {cardData.date && <p className="event-date">{cardData.date}</p>}
-            {cardData.venue && <p className="event-place">{cardData.venue}</p>}
+            <EventHeading
+              title={cardData.title}
+              lines={2}
+              height={Utilities.mobilecheck() ? 19 : Utilities.mobileAndTabletcheck() ? 20 : 20}
+            />
+            {cardData.date &&
+              <Ellipsis
+                title={cardData.date}
+                lines={1}
+                customClass="event-date"
+                height={Utilities.mobilecheck() ? 17 : Utilities.mobileAndTabletcheck() ? 17 : 17}
+              />
+            }
+            {cardData.venue &&
+              <Ellipsis
+                title={cardData.venue}
+                lines={1}
+                customClass="event-place"
+                height={Utilities.mobilecheck() ? 17 : Utilities.mobileAndTabletcheck() ? 17 : 17}
+              />
+            }
             <span className={`${cardData.type === 'event' ? `event` : ``}`}>
               {cardData.category}
             </span>
@@ -53,8 +74,8 @@ const Card = props => {
             {cardData.type === 'event' || cardData.type === 'attractions' ? (
               <button type="button">Buy Tickets</button>
             ) : (
-              <button type="button">Read More</button>
-            )}
+                <button type="button">Read More</button>
+              )}
           </div>
         </div>
       </div>
