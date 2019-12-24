5b153c657eb248df8bcea6d2219da3cc6440fdd5
Shubham Soni <shubham.soni@ranosys.com>
2019-12-24 17:47:51 +0530

    Update index.js

--- web/src/scenes/App/Navigator/index.js
+++ web/src/scenes/App/Navigator/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import routes from '../routes';
 
 const Routes = ({ match }) => {
   return (
+
     <Switch>
       {routes.map(({ path, exact, component: C, ...rest }) => (
         <Route
           key={path}
           path={path}
           exact={exact}
-          render={props => <C {...props} {...rest} data={props.data} />}
+          render={(props) => (
+            <C {...props} {...rest} data={props.data} />
+          )}
         />
       ))}
       {/*  <Route render={(props) => <NoMatch {...props} />} /> */}
     </Switch>
+
   );
 };
 
